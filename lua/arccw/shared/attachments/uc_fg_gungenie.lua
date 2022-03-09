bd32f6841b256ec2aa6067b37bfbbea02eaef6b6
ColonelRedips <gadunka.m203@gmail.com>
2022-03-09 00:55:41 -0500

    Gun Aladdin

---
+++ lua/arccw/shared/attachments/uc_fg_gungenie.lua
@@ -0,0 +1,49 @@
+att.PrintName = "GUN GENIE Firearm Enhancer"
+
+att.Icon = Material("entities/att/arccw_uc_forcedresettrigger.png", "mips smooth")
+att.Description = "The notrious AliBubba strikes again."
+att.Desc_Pros = {
+    "uc.auto"
+}
+att.Desc_Cons = {
+    "uc.frcd_visrec"
+}
+att.Desc_Neutrals = {
+}
+att.Slot = "uc_fg"
+att.AutoStats = true
+att.SortOrder = 3
+
+att.Hook_Compatible = function(wep)
+    if wep:GetIsManualAction() then return false end
+
+    -- for i, v in pairs(wep.Firemodes) do
+    --     if !v then continue end
+    --     if !v.Mode then continue end
+    --     if v.Mode == 2 then
+    --         -- Not available if gun has automatic firemode
+    --         return false
+    --     -- elseif v.Mode < 0 then
+    --     --     -- Use burst variant
+    --     --     return false
+    --     end
+    -- end
+end
+
+att.Override_Firemodes_Priority = 100
+att.Override_Firemodes = {
+    {
+        PrintName = "GENIE",
+        Mode = 2,
+    },
+    {
+        Mode = 0
+    }
+}
+
+att.Mult_RPM = 1.666
+att.Mult_RecoilSide = 3.60
+att.Mult_VisualRecoilMult = 2
+att.Mult_MalfunctionMean = .666
+
+att.AttachSound = "arccw_uc/common/gunsmith/internal_modification.ogg"
