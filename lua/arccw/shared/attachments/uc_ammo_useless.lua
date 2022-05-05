d7c27ced7916a63ca69cff1019c73c9a8970d9ee
VolatileIvory <101301754+VolatileIvory@users.noreply.github.com>
2022-05-05 14:58:39 -0700

    Sabotaged ammo. Because why not.
    Definitely not untested and stolen from BlackMod.

---
+++ lua/arccw/shared/attachments/uc_ammo_useless.lua
@@ -0,0 +1,19 @@
+att.PrintName = "\"SAB\" Sabotaged Rounds"
+att.AbbrevName = "\"SAB\" Sabotaged"
+
+att.SortOrder = 4
+att.Icon = Material("entities/att/arccw_uc_ammo_generic.png", "mips smooth")
+att.Description = "Sabotaged ammunition with no powder load. Useful for trolling your friends or annoying everyone nearby with constant malfunction fix sounds."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.Desc_Neutrals = {
+}
+att.Slot = "uc_ammo"
+
+att.Override_Malfunction = true
+att.Mult_MalfunctionMean = 0
+att.Mult_MalfunctionVariance = 99
+
+att.AutoStats = true
