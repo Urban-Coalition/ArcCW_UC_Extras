fb23f28b30badd7a5f70a026bb3c885f6cde0e74
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-05-06 10:14:51 +1000

    Minor balance

--- lua/arccw/shared/attachments/ud_m16_mag_50beo_24.lua
+++ lua/arccw/shared/attachments/ud_m16_mag_50beo_24.lua
@@ -2,7 +2,7 @@ att.PrintName = ".50 Beowulf 24-Round Dual-Stack Mag"
 att.AbbrevName = "24-Round Dual-Stack Mag"
 
 att.Icon = Material("entities/att/acwatt_ud_m16_mag_60.png", "smooth mips")
-att.Description = "Quad-Stack 5.56mm magazines converted to load .50 Beowulf rounds in a Double-Stack stack. Loading something like this is well outside of plausability."
+att.Description = "Quad-Stack 5.56mm magazines converted to load .50 Beowulf rounds in a Double-Stack stack. Dual stacking a round not designed for it leads to abysmal reliability."
 att.Desc_Pros = {}
 att.Desc_Cons = {}
 att.Desc_Neutrals = {}
@@ -18,6 +18,11 @@ att.Override_ClipSize = 24
 att.Mult_SightTime = 1.2
 att.Mult_ReloadTime = 1.3
 att.Mult_Sway = 2.25
+
+
+att.Override_Malfunction = true
+att.Mult_MalfunctionMean = 0.5
+att.Mult_MalfunctionVariance = 1.5
 
 att.Mult_SpeedMult = 0.95
 
