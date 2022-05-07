34c4735723b2d6f2417b1af06d17b3308f1f4daa
ColonelRedips <gadunka.m203@gmail.com>
2022-05-07 11:16:11 -0400

    Uzi Drum is fixed. Grendel gets the shaft because of M16 being janky as shit

--- lua/arccw/shared/attachments/ud_uzi_mag_45_60.lua
+++ lua/arccw/shared/attachments/ud_uzi_mag_45_60.lua
@@ -1,5 +1,5 @@
-att.PrintName = "Uzi .45 60-Round C-Mag"
-att.AbbrevName = ".45 60-Round C-Mag"
+att.PrintName = "Uzi 60-Round C-Mag"
+att.AbbrevName = "60-Round C-Mag"
 
 if !GetConVar("arccw_truenames"):GetBool() then
     att.PrintName = "STAP 60-Round C-Mag"
@@ -9,36 +9,32 @@ end
 att.SortOrder = 100
 att.Icon = Material("entities/att/acwatt_ud_uzi_mag_100.png", "smooth mips")
 att.Description = "Custom high-capacity modification of the 100-round 9mm C-Mag designed to chamber .45 ACP rounds. Comes at the expense of everything else."
-att.Desc_Pros = {
-}
-att.Desc_Cons = {
-    "uc.jam"
-}
-att.Desc_Neutrals = {
-}
+att.Desc_Pros = {}
+att.Desc_Cons = { "uc.jam" }
+att.Desc_Neutrals = {}
 att.Slot = "ud_uzi_mag"
 
 att.AutoStats = true
 
 att.Mult_SightTime = 1.66
---att.Mult_Recoil = 0.9
 att.Mult_ReloadTime = 1.66
 att.Mult_SpeedMult = 0.85
-att.Mult_DrawTime = 1.25
+att.Mult_DrawTime = 1.33
 att.Override_ClipSize = 60
+att.Override_ClipSize_Priority = 2
 att.Mult_Sway = 2
 att.Mult_ShootSpeedMult = 0.85
 
-att.ActivateElements = {"ud_uzi_100_mag"}
+att.ActivateElements = {"ud_uzi_mag_100"}
 
 --att.Override_Jamming = true
 att.Override_Malfunction = true
-att.Mult_MalfunctionMean = 0.66
+att.Mult_MalfunctionMean = 0.88
 att.Mult_MalfunctionVariance = 1.88
 
 att.Hook_SelectReloadAnimation = function(wep, anim)
     return anim .. "_100"
 end
 
-att.ExcludeFlags = {"uzi_45","uzi_22", "uzi_41"}
+att.RequireFlags = {"uzi_45"}
 att.HideIfBlocked = true
