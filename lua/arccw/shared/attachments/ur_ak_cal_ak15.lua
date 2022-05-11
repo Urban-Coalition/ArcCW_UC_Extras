d177a5f672699c738fe3dd7b5e3df7069dfb27f4
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-05-11 18:13:32 +1000

    when you forget a hyphen

--- lua/arccw/shared/attachments/ur_ak_cal_ak15.lua
+++ lua/arccw/shared/attachments/ur_ak_cal_ak15.lua
@@ -1,7 +1,7 @@
 att.PrintName = "AK-15 7.62x39mm Receiver"
 att.AbbrevName = "AK-15 Receiver"
 att.Icon = Material("entities/att/ur_ak/recievers/ak12.png", "mips smooth")
-att.Description = "Modern stamped receiver derived from the AK12/400 rifle. Shares the same glossy finish and rails; otherwise, it functions similarly to regular stamped AKMs."
+att.Description = "Modern stamped receiver derived from the AK-12/400 rifle. Shares the same glossy finish and rails; otherwise, it functions similarly to regular stamped AKMs."
 att.Desc_Neutrals = {
     "uc.cosmetic"
 }
