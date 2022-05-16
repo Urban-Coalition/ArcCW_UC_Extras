2ace2716b5c670283b98e47273eff30508fa6bd0
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-05-16 17:00:04 +1000

    You can get Glocks in .460 Rowland too!

---
+++ lua/arccw/shared/attachments/ud_glock_caliber_460.lua
@@ -0,0 +1,70 @@
+att.PrintName = "Glock .460 Rowland Conversion"
+att.AbbrevName = ".460 Rowland"
+
+if !GetConVar("arccw_truenames"):GetBool() then
+    att.PrintName = "GEN3 .460 Conversion"
+end
+
+att.SortOrder = 70
+att.Icon = Material("entities/att/acwatt_ud_glock_caliber.png", "smooth mips")
+att.Description = "The .460 Rowland is a proprietary cartridge which attains true .44 Magnum level velocities when fired from a number of popular semi-automatic pistols, revolvers, and rifles."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.Desc_Neutrals = {
+}
+att.Slot = "ud_glock_caliber"
+
+att.AutoStats = true
+
+att.Override_Trivia_Calibre = ".460 Rowland"
+
+att.Mult_Damage = 1.275
+att.Mult_DamageMin = 0.88
+att.Mult_Penetration = 10
+att.Mult_Range = 1.25
+att.Mult_Precision = 1.25
+att.Mult_Recoil = 1.10
+att.Mult_MalfunctionMean = 1.05
+att.Override_PhysBulletMuzzleVelocity = 3000
+
+att.Mult_ClipSize = 0.76
+
+att.Mult_RPM = 0.7619
+
+
+att.Hook_GetShootSound = function(wep, sound)
+    if wep:GetBuff_Override("Silencer") then
+        return "weapons/arccw_ud/glock/fire_supp_45.ogg" -- Placeholder
+    else
+        return "weapons/arccw_ud/glock/fire_45.ogg" -- Not Placeholder
+    end
+end
+
+att.Hook_GetDistantShootSoundOutdoors = function(wep, distancesound)
+    if wep:GetBuff_Override("Silencer") then
+        -- fallback to script
+    else
+        return { "weapons/arccw_ud/glock/fire_dist_45.ogg" }
+    end
+end
+
+local path = ")^weapons/arccw_ud/glock/"
+att.Hook_GetDistantShootSoundIndoors = function(wep, distancesound)
+    if wep:GetBuff_Override("Silencer") then
+        -- fallback to script
+    else
+        return {
+            path .. "fire-dist-int-heavy-01.ogg",
+            path .. "fire-dist-int-heavy-02.ogg",
+            path .. "fire-dist-int-heavy-03.ogg",
+            path .. "fire-dist-int-heavy-04.ogg",
+            path .. "fire-dist-int-heavy-05.ogg",
+            path .. "fire-dist-int-heavy-06.ogg"
+        }
+    end
+end
+
+att.GivesFlags = {"cal_subsonic"}
+att.ExcludeFlags = {"powder_subsonic"}
