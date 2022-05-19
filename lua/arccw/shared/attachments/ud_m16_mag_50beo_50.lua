83efb62b2a23b90930178af4a90dd286aec162eb
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-05-19 16:52:13 +1000

    accidentally deleted this ages ago lol

---
+++ lua/arccw/shared/attachments/ud_m16_mag_50beo_50.lua
@@ -0,0 +1,22 @@
+att.PrintName = ".50 Beowulf 40-Round C-Mag"
+att.AbbrevName = "40-Round C-Mag"
+att.SortOrder = 100
+att.Icon = Material("entities/att/acwatt_ud_m16_mag_100.png", "smooth mips")
+att.Description = "\"REDIPS PUT SOMETHING FUNNY HERE BEFORE THE WS PUSH!\" Taskmaster said. \"No\" Redips replied calmly."
+att.Desc_Pros = {}
+att.Desc_Cons = {
+    "uc.jam"
+}
+att.Desc_Neutrals = {}
+att.Slot = "ud_m16_mag"
+att.AutoStats = true
+att.HideIfBlocked = true
+att.Override_ClipSize = 40
+att.Mult_SightTime = 1.5
+att.Mult_ReloadTime = 1.5
+att.Mult_Sway = 3
+att.Mult_SpeedMult = 0.9
+att.Mult_ShootSpeedMult = 0.8
+att.Mult_DrawTime = 1.25
+att.Mult_HolsterTime = 1.25
+att.ActivateElements = {"ud_m16_mag_100"}
