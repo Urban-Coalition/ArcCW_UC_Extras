9a835b83e5e773377469dcf4431086942004d4ff
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-05-20 10:25:47 +1000

    Add files via upload

---
+++ lua/arccw/shared/attachments/uc_fg_fieldstrip.lua
@@ -0,0 +1,23 @@
+att.PrintName = "Field Strip"
+att.AbbrevName = "Field Strip"
+att.Icon = Material("entities/att/arccw_uc_tp_fieldstrip.png", "smooth mips")
+att.SortOrder = 20
+att.Description = "Remove \“Unnecessary\” parts to reduce weight in the gun, greatly improving the handling of the weapon."
+att.Desc_Pros = {}
+att.Desc_Cons = {}
+att.Desc_Neutrals = {}
+
+att.Slot = "uc_fg"
+
+att.AutoStats = true
+
+att.Mult_SightTime = 0.8
+att.Mult_SpeedMult = 0.8
+att.Mult_ShootSpeedMult = 0.8
+att.Mult_DrawTime = 0.8
+att.Mult_HolsterTime = 0.8
+att.Mult_SightedSpeedMult = 1.2
+att.Mult_SpeedMult = 1.2
+
+att.Mult_MalfunctionMean = 1.2
+att.Mult_Recoil = 1.2
