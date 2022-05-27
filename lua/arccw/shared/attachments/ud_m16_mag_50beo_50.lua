79d15e7aa4d5292ec0df2586b95d16c21ce64988
ColonelRedips <gadunka.m203@gmail.com>
2022-05-27 09:42:10 -0400

    Beo with some titties

--- lua/arccw/shared/attachments/ud_m16_mag_50beo_50.lua
+++ lua/arccw/shared/attachments/ud_m16_mag_50beo_50.lua
@@ -11,6 +11,7 @@ att.Desc_Neutrals = {}
 att.Slot = "ud_m16_mag"
 att.AutoStats = true
 att.HideIfBlocked = true
+
 att.Override_ClipSize = 40
 att.Mult_SightTime = 1.5
 att.Mult_ReloadTime = 1.5
@@ -19,4 +20,17 @@ att.Mult_SpeedMult = 0.9
 att.Mult_ShootSpeedMult = 0.8
 att.Mult_DrawTime = 1.25
 att.Mult_HolsterTime = 1.25
+
 att.ActivateElements = {"ud_m16_mag_100"}
+att.RequireFlags = {"m16_50beo"}
+att.ExcludeFlags = {"m16_usas", "m16_9mm"}
+
+att.Hook_SelectReloadAnimation = function(wep, anim)
+    return anim .. "_100"
+end
+
+att.Hook_TranslateAnimation = function(wep, anim)
+    if (anim == "fix") then
+        return anim .. "_100"
+    end
+end
