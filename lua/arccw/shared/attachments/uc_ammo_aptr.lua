275de503489fcf74e0bae284eed9b917650dbf16
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-06-10 10:24:38 +1000

    Armour-Piercing Tracer acronym change + restricted to rifle and sniper ammo

--- lua/arccw/shared/attachments/uc_ammo_aptr.lua
+++ lua/arccw/shared/attachments/uc_ammo_aptr.lua
@@ -1,8 +1,8 @@
-att.PrintName = "\"APTR\" Armor-piercing Tracer Rounds"
-att.AbbrevName = "\"APTR\" Armor-piercing Tracer"
+att.PrintName = "\"AP-T\" Armor-piercing Tracer Rounds"
+att.AbbrevName = "\"AP-T\" Armor-piercing Tracer"
 att.SortOrder = 1
 att.Icon = Material("entities/att/arccw_uc_ammo_generic.png", "mips smooth")
-att.Description = "Bullets designed to penetrate body armor, usually built around a penetrator of hardened steel, tungsten, or tungsten carbide.\nUpon impact on a hard target, the case is destroyed, but the penetrator continues its motion and penetrates the target."
+att.Description = "Bullets designed to penetrate body armor and provide a clearer visual indication of shot trajectory. Usually built around a penetrator of hardened steel, tungsten, or tungsten carbide with a tracer compound in the rear cavity or for larger-caliber projectiles, the tracer may instead be contained within an extension of the rear sealing plug.\nUpon impact on a hard target, the case is destroyed, but the penetrator continues its motion and penetrates the target."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -39,6 +39,12 @@ att.Override_Tracer = "arccw_uc_tracer"
 
 att.Hook_Compatible = function(wep)
     if wep:GetIsShotgun() then
+        return false
+    end
+end
+
+att.Hook_Compatible = function(wep)
+    if !(wep.Primary.Ammo == "ar2" or wep.Primary.Ammo == "SniperPenetratedRound") then
         return false
     end
 end
