9c3d3a64f59117b0f353dd2d20c8c2ef0eca372e
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-06-28 22:23:52 +1000

    Added hair trigger

---
+++ lua/arccw/shared/attachments/uc_fg_hair_trigger.lua
@@ -0,0 +1,16 @@
+att.PrintName = "Hair Trigger"
+att.Icon = Material("entities/att/arccw_uc_forcedresettrigger.png", "mips smooth")
+att.Description = "An extremely light trigger with short length of pull, designed for use in competition shooting."
+att.Desc_Pros = {
+    "-100% Trigger delay time",
+}
+att.Desc_Cons = {
+}
+att.Desc_Neutrals = {
+}
+att.AutoStats = true
+att.SortOrder = 2
+att.Slot = {"uc_fg"}
+
+att.Mult_TriggerDelayTime = 0
+att.Mult_Recoil = 1.05
