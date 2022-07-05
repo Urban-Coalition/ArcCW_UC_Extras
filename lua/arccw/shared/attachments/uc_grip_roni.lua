179c02f585056553685ad34e48d180ced9647c41
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-07-05 19:15:07 +1000

    PISTOL EXCLUSIVE FOREGRIP CUNT!

---
+++ lua/arccw/shared/attachments/uc_grip_roni.lua
@@ -0,0 +1,24 @@
+att.PrintName = "Roni Foregrip"
+att.Icon = Material("entities/att/acwatt_uc_grip_roni.png", "mips smooth")
+att.Description = "Folding foregrip from a Glock RONI carbine kit. Provides greater recoil control at the cost of handling."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.SortOrder = 999
+
+att.Slot = "tac_pistol"
+
+att.LHIK = true
+
+att.Model = "models/weapons/arccw/mifl_atts/fas2_roni_grip.mdl"
+
+att.Mult_Recoil = 0.75
+att.Mult_SightTime = 1.15
+att.Mult_SightedSpeedMult = 0.90
+att.Mult_ReloadTime = 1.10
+
+att.ModelOffset = Vector(-3.75, 0, -1.2)
+
+att.Override_HoldtypeActive = "smg"
