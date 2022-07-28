8d82164b98c982f3aa5f16f23fbb9970b9f20cf1
Cylowalker <76599609+Cylowalkee@users.noreply.github.com>
2022-07-29 00:33:19 +0300

    Raptrr in Extras
    tasky balance this man.

---
+++ lua/arccw/shared/attachments/uc_stock_raptor.lua
@@ -0,0 +1,18 @@
+att.PrintName = "Raptor Tactical Stock"
+att.AbbrevName = "Raptor Stock"
+att.Icon = nil
+att.Description = "Mitigates the recoil disadvantage typical of most pistols, but extends the weapon's profile substantially.\n\nYou probably shouldn't let the feds know you have this."
+att.Slot = "go_stock_pistol_bt"
+att.AutoStats = true
+
+att.Model = "models/weapons/arccw/atts/uc_stock_raptor.mdl"
+
+att.ModelScale = Vector(0.75, 0.75, 0.75)
+att.ModelOffset = Vector(0.4, 0, 0)
+
+att.Mult_Recoil = .70
+att.Mult_RecoilSide = .5
+att.Mult_Sway = .5
+att.Mult_SightTime = 1.3
+att.Mult_DrawTime = 1.4
+att.Add_BarrelLength = 20
